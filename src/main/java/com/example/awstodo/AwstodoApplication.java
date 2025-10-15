8282e60b5ea4dbde988b022e23e58fcbdc6a6ffa
yun-jun-hyengh <77119415+yun-jun-hyengh@users.noreply.github.com>
2025-10-15 15:18:25 +0900

    Initial commit

---
+++ src/main/java/com/example/awstodo/AwstodoApplication.java
@@ -0,0 +1,13 @@
+package com.example.awstodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwstodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwstodoApplication.class, args);
+	}
+
+}
